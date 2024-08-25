3f9289a0a0be92a0fe4e99e93e285205098e9dd2
unknown <you@example.com>
2024-08-25 21:47:21 +0545

    Updated Code

--- HospitalManagementSystem/src/main/java/com/Hospital/Management/System/controller/PatientsController.java
+++ HospitalManagementSystem/src/main/java/com/Hospital/Management/System/controller/PatientsController.java
@@ -2,6 +2,7 @@ package com.Hospital.Management.System.controller;
 
 import java.util.List;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -10,7 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.Hospital.Management.System.Entity.Patient;
 import com.Hospital.Management.System.Repository.PatientsRepository;
-
+@CrossOrigin(origins = "http://localhost:4200")
 @RestController
 @RequestMapping("/api/v0")
 public class PatientsController {
@@ -25,6 +26,7 @@ public class PatientsController {
 	public Patient CreatePatient(@RequestBody Patient patient) {
 		return patientsRepositry.save(patient);
 	}
+	@CrossOrigin(origins = "http://localhost:4200")
 	@GetMapping
 	public List<Patient>getAllPatient(){
 		return patientsRepositry.findAll();
--- HospitalManagementSystem/src/main/java/com/Hospital/Management/System/doclogin/controller/AppointmentController.java
+++ HospitalManagementSystem/src/main/java/com/Hospital/Management/System/doclogin/controller/AppointmentController.java
@@ -1,15 +1,23 @@
 package com.Hospital.Management.System.doclogin.controller;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
+import javax.management.AttributeNotFoundException;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import com.Hospital.Management.System.doclogin.entity.Appointment;
 import com.Hospital.Management.System.doclogin.repository.AppointmentsRepository;
-
+@CrossOrigin(origins = "http://localhost:4200")
 @RestController
 @RequestMapping("/api/v1")
 public class AppointmentController {
@@ -32,14 +40,26 @@ public class AppointmentController {
 	}
 
 
-	@PostMapping("/insert")
+	@PostMapping("/appointments")
 	public Appointment createAppointment(@RequestBody Appointment appointment) {
 		return appointmentsRepository.save(appointment);
 	}
-	@GetMapping
+	@GetMapping("/appointments")
 	public List<Appointment>getAllAppointment(){
 		return appointmentsRepository.findAll();
 	}
+	
+	
+	@DeleteMapping("/appointments/{id}")
+	public ResponseEntity<Map<String, Boolean>> deleteAppointment(@PathVariable long id) throws AttributeNotFoundException {
+	    Appointment appointment = appointmentsRepository.findById(id).orElseThrow(() -> new AttributeNotFoundException("Appointment not found for this id  " + id));
+
+	    appointmentsRepository.delete(appointment);
+	    Map<String, Boolean> response = new HashMap<String,Boolean>();
+	    response.put("deleted", Boolean.TRUE);
+	    return ResponseEntity.ok(response);
+	}
+
 
 	
 }
