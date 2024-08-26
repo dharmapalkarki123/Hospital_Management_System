5600f3e74a4ccc1edfa921dfe42d57c65720bc15
unknown <you@example.com>
2024-08-26 21:21:23 +0545

    Change commit

--- HospitalManagementSystem/src/main/java/com/Hospital/Management/System/controller/PatientsController.java
+++ HospitalManagementSystem/src/main/java/com/Hospital/Management/System/controller/PatientsController.java
@@ -1,9 +1,16 @@
 package com.Hospital.Management.System.controller;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
+import javax.management.AttributeNotFoundException;
+
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,6 +18,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.Hospital.Management.System.Entity.Patient;
 import com.Hospital.Management.System.Repository.PatientsRepository;
+import com.Hospital.Management.System.doclogin.entity.Appointment;
 @CrossOrigin(origins = "http://localhost:4200")
 @RestController
 @RequestMapping("/api/v0")
@@ -22,14 +30,24 @@ public class PatientsController {
 		this.patientsRepositry = patientsRepositry;
 	}
 
-	@PostMapping("/insert")
+	@PostMapping("/patients")
 	public Patient CreatePatient(@RequestBody Patient patient) {
 		return patientsRepositry.save(patient);
 	}
-	@CrossOrigin(origins = "http://localhost:4200")
-	@GetMapping
+	@GetMapping("/patients")
 	public List<Patient>getAllPatient(){
 		return patientsRepositry.findAll();
 	}
 
+	@DeleteMapping("/patients/{id}")
+	public ResponseEntity<Map<String, Boolean>> deletePatient(@PathVariable long id) throws AttributeNotFoundException {
+	    Patient patient = patientsRepositry.findById(id).orElseThrow(() -> new AttributeNotFoundException("Patient not found for this id  " + id));
+
+	    patientsRepositry.delete(patient);
+	    Map<String, Boolean> response = new HashMap<String,Boolean>();
+	    response.put("deleted", Boolean.TRUE);
+	    return ResponseEntity.ok(response);
+	}
+	
+	
 }
