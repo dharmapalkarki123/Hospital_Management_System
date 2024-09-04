9f4423c50b388e9f00c99a24628def962b0eeecf
unknown <you@example.com>
2024-09-04 20:11:11 +0545

    Backend Commit

--- HospitalManagementSystem/src/main/java/com/Hospital/Management/System/controller/PatientsController.java
+++ HospitalManagementSystem/src/main/java/com/Hospital/Management/System/controller/PatientsController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -38,7 +39,39 @@ public class PatientsController {
 	public List<Patient>getAllPatient(){
 		return patientsRepositry.findAll();
 	}
+	
+	@GetMapping("/patients/{id}")
+	public ResponseEntity<Patient> getPatientById(@PathVariable Long id) throws AttributeNotFoundException {
+		
+		Patient patient = patientsRepositry.findById(id)
+				.orElseThrow(() -> new AttributeNotFoundException("ABCD" + id));
+		
+		return ResponseEntity.ok(patient);
+	}
+	
+	
+	
 
+	@PutMapping("/patients/{id}")
+	public ResponseEntity<Patient> updatePatient(@PathVariable Long id, @RequestBody Patient patientDetails) throws AttributeNotFoundException{
+		
+		Patient patient = patientsRepositry.findById(id)
+				.orElseThrow(() -> new AttributeNotFoundException("ABCD" + id));
+		
+		patient.setAge(patientDetails.getAge());
+		patient.setName(patientDetails.getName());
+		patient.setBlood(patientDetails.getBlood());
+		patient.setDose(patientDetails.getDose());
+		patient.setFees(patientDetails.getFees());
+		patient.setPrescription(patientDetails.getPrescription());
+		patient.setUrgency(patientDetails.getUrgency());
+		
+		
+		Patient updatedPatient = patientsRepositry.save(patient);
+		return ResponseEntity.ok(updatedPatient);
+	}
+	
+	
 	@DeleteMapping("/patients/{id}")
 	public ResponseEntity<Map<String, Boolean>> deletePatient(@PathVariable long id) throws AttributeNotFoundException {
 	    Patient patient = patientsRepositry.findById(id).orElseThrow(() -> new AttributeNotFoundException("Patient not found for this id  " + id));
--- HospitalManagementSystem/src/main/java/com/Hospital/Management/System/doclogin/controller/MedicineController.java
+++ HospitalManagementSystem/src/main/java/com/Hospital/Management/System/doclogin/controller/MedicineController.java
@@ -1,9 +1,18 @@
 package com.Hospital.Management.System.doclogin.controller;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
+import javax.management.AttributeNotFoundException;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,6 +20,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.Hospital.Management.System.Entity.Patient;
 import com.Hospital.Management.System.doclogin.entity.Medicine;
 import com.Hospital.Management.System.doclogin.repository.MedicinesRepository;
+@CrossOrigin(origins = "http://localhost:4200")
 @RestController
 @RequestMapping("/api/v2")
 public class MedicineController {
@@ -28,14 +38,63 @@ public class MedicineController {
 	public void setMedicinesRepository(MedicinesRepository medicinesRepository) {
 		this.medicinesRepository = medicinesRepository;
 	}
-	@PostMapping("/insert")
+	@PostMapping("/medicines")
 	public Medicine createMedicines(@RequestBody Medicine medicine) {
 		return medicinesRepository.save(medicine);
 	}
-	@GetMapping
+	@GetMapping("/medicines")
 	public List<Medicine>getAllPatient(){
 		return medicinesRepository.findAll();
 	}
 
+	
+	@GetMapping("/medicines/{id}")
+	public ResponseEntity<Medicine> getPatientById(@PathVariable Long id) throws AttributeNotFoundException {
+		
+		Medicine medicine = medicinesRepository.findById(id)
+				.orElseThrow(() -> new AttributeNotFoundException("ABCD" + id));
+		
+		return ResponseEntity.ok(medicine);
+	}
+
+	
+	@PutMapping("/medicines/{id}")
+	public ResponseEntity<Medicine> updateMedicine(@PathVariable Long id, @RequestBody Medicine medicineDetails) throws AttributeNotFoundException{
+		
+		Medicine medicine = medicinesRepository.findById(id)
+				.orElseThrow(() -> new AttributeNotFoundException("ABCD" + id));
+		
+		medicine.setDrugName(medicineDetails.getDrugName());
+		medicine.setStock(medicineDetails.getStock());
+	
+		
+		Medicine updatedMedicine = medicinesRepository.save(medicine);
+		return ResponseEntity.ok(updatedMedicine);
+	}
+	
+	
+	
+	
+	@DeleteMapping("/medicines/{id}")
+	public ResponseEntity<Map<String,Boolean>> deleteMedicine(@PathVariable Long id) throws AttributeNotFoundException{
+		
+		Medicine medicine = medicinesRepository.findById(id)
+				.orElseThrow(() -> new AttributeNotFoundException("ABCD" + id));
+		
+		medicinesRepository.delete(medicine);
+		Map<String, Boolean> response = new HashMap<>();
+		response.put("deleted", Boolean.TRUE);
+		return ResponseEntity.ok(response);
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 	
 }
